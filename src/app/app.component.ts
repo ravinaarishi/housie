a1f0a16b62aed1bb705a2cca2e0c2bb5dfe88ac1
Rishi Wadhwani <iRishi@Rishis-MacBook-Pro-2.local>
2020-04-11 04:35:39 -0400

    game done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HOUSIE_TEXT } from './housie-text';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'housie-app';
+export class AppComponent implements OnInit {
+  housieText = HOUSIE_TEXT;
+  ticketInput;
+  generatedTickets;
+  generatedNumbers;
+  showTickets = false;
+  showHousieBoard = false;
+  showGameOver = false;
+  currentNumber;
+  currentText;
+  calledNumbers = [];
+  tambola;
+  oneToEight = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  oneToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  snackBarRef;
+  @ViewChild('ticketsDiv') ticketDivElement: ElementRef;
+  @ViewChild('housieBoardOuterDiv') housieBoardDivElement: ElementRef;
+  @ViewChild('housieBoard') housieBoardElement: ElementRef;
+
+    constructor(private snackBar: MatSnackBar) {
+      this.tambola = require('tambola-generator');
+      this.ticketInput = new FormControl('');
+    }
+
+    public ngOnInit(): void {
+    }
+
+    public genrateTickets() {
+      this.showHousieBoard = false;
+      const num = +this.ticketInput.value;
+      this.generatedTickets = this.tambola.getTickets(num);
+      this.showTickets = true;
+      setTimeout(() => this.ticketDivElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' }));
+    }
+
+    public openHousieBoard() {
+      this.reset();
+      this.generatedNumbers = this.tambola.getDrawSequence();
+      this.showHousieBoard = true;
+      setTimeout(() => this.housieBoardDivElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' }));
+    }
+
+    public callNumber() {
+      if (this.calledNumbers.length < this.generatedNumbers.length) {
+        this.currentNumber = this.generatedNumbers[this.calledNumbers.length];
+        this.currentText = this.housieText[this.currentNumber - 1];
+        this.calledNumbers.push(this.currentNumber);
+        setTimeout(() => this.housieBoardElement.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' }));
+      } else {
+        this.snackBarRef = this.snackBar.open('Game Over..!!', 'RESET');
+        this.snackBarRef.onAction().subscribe(() => {
+          this.reset();
+        });
+        this.showGameOver = true;
+      }
+    }
+
+    public reset() {
+      this.currentNumber = null;
+      this.currentText = null;
+      this.showTickets = false;
+      this.showHousieBoard = false;
+      this.showGameOver = false;
+      this.calledNumbers = [];
+      this.generatedNumbers = null;
+      this.ticketInput = new FormControl('');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatTableModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
